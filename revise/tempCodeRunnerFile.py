7f3397f8d144996440e29c4484cf6f5b5332a96e
ZiPPO7777 <iamrealzippo@gmail.com>
2025-07-16 19:36:36 +0530

    Little Exercise

--- revise/tempCodeRunnerFile.py
+++ revise/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
 # more about list
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1,2,23,4,5,52,1]
+
+find1 = numbers.index(1)
+
+find2 = numbers.index(1, find1 + 1)
+
+print(numbers.index(1, find2 + 1))
