23485561ef0c82237679ba6d862788e93b83e55c
ZiPPO7777 <iamrealzippo@gmail.com>
2025-07-20 11:38:20 -0700

    dictionary add data - done

---
+++ Python/chapter7/a.py
@@ -0,0 +1,15 @@
+user_info = {
+    'name': 'ZiPPO',
+    'age': 21
+}
+
+
+user_info['Fav_song'] = ['yummy', 'descpacito']
+
+user_info['Fav_movie'] = ['Krish', 'Raone']
+
+
+popped_item = user_info.popitem()
+
+print(user_info)
+
