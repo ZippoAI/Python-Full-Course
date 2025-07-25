9fdaabe9cbe60ccfff9b513402e1857284f9e441
ZiPPO7777 <iamrealzippo@gmail.com>
2025-07-25 13:25:03 +0530

    Exercise Done

--- Python/chapter9-(Comprehension)/T132_Exercise.py
+++ Python/chapter9-(Comprehension)/T132_Exercise.py
@@ -1,9 +1,5 @@
-def num_string(n):
-    numstring = [str(i) for i in n if type(i) == int or type(i) == float]
-    return numstring
-
-input_value = [True, False, 'zippo', [1,2,3], 1, 1.0, 3]
-print(num_string(input_value))
-
-
-
+def int_or_float_finder(l):
+    result = [i for i in l if type(i)== int or type(i)== float]
+    return  list(result)
+inpt  = [1,3,4, True , 'hello', 3.4, 1.0]
+print(int_or_float_finder(inpt))
