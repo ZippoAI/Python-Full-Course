e3df807aac7f7320c8cf987e946c6dda34552e1a
ZiPPO7777 <iamrealzippo@gmail.com>
2025-07-31 22:15:01 +0530

    a

--- Python/chapter13/T157_ZipPart2.py
+++ Python/chapter13/T157_ZipPart2.py
@@ -10,5 +10,24 @@ print(final)
 
 li1 , li2 = zip(*final)
 
-print(li1)
-print(li2)
+print("li1 :" , li1)
+print("li2 :" ,li2)
+
+l3 = [a for a in range(1,11) if a%2 ==0]
+l4 = [a for a in range(1,11) if a%2 ==1]
+
+new_list = []
+for i in zip(l3,l4):
+    new_list.append(max(i))
+print(new_list)
+
+
+# new_list = []
+# for i in range(len(l3)):  
+#     if l3[i] > l4[i]:
+#         new_list.append(l3[i])
+#     else:
+#         new_list.append(l4[i])
+# print(new_list)
+
+
