b137696fd74c373f9240dbc3114e21f4c0338962
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-04 02:58:29 +0530

    bug fix

---
+++ chapter0/loops/for_loop/1-foorLoop.py
@@ -0,0 +1,24 @@
+# number = [1234]
+
+# for i in number:
+#     print(i)
+
+
+  
+# for i in range(1,6):
+#     print(i)
+# print('for loop ended')
+
+# for i in range(1,5):
+#     if i == 3:
+#         continue
+#     print(i)
+        
+
+
+
+
+for i in range(1,5):
+    if i==2:
+        continue
+    print(i)
