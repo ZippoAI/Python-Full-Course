891c8061d1481eddb40f7a553841aa1b7f043eba
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-09 22:47:19 +0530

    doc string Python

---
+++ Python/chapter13/T164MoreAboutList.py
@@ -0,0 +1,12 @@
+'''
+What are doc-strings
+how to write docstring
+how to see doc-strings
+what is help function
+'''
+
+def add(a,b):
+    '''This function takes 2 numbers and returns their sum'''
+    return a + b
+
+
