54b41b509ff491d56be2fc5fb078bc21f5d811b0
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-18 01:47:01 +0530

    closure practice

--- Python/chapter14/T168-CloserPractice.py
+++ Python/chapter14/T168-CloserPractice.py
@@ -1,1 +1,14 @@
-asd
+# Function Returning Function (Closure) Practice
+
+def to_power(x):
+    def calculate_power(n):
+        return n**x
+    return calculate_power
+
+cube = to_power(3)
+print(cube(5))
+
+
+square = to_power(2)
+
+print(square(4))
