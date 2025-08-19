a277c3f1393befed9259259c4f1e3636c3627651
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-19 11:10:44 +0530

    pass function as argument

--- Python/chapter14/T166PassFunction.py
+++ Python/chapter14/T166PassFunction.py
@@ -1,16 +1,22 @@
 # Pass a function as an argument
-
+def three_power(a):
+    return a**3
 
 def square(a):
     return a**2
 
-
-l2= [1,2,3,4,5]
-
-def func(func, l):
-    new_l = [func(i) for i in l]
-    return new_l
-
-print(func(square, l2))
-
+l = [1,2,3,4,5]
   
+print(list(map(square, l)))
+
+#instead of using map we can do this too
+def map_func(func, a):
+    empty = []
+    for i in a:
+        empty.append(func(i))
+    return empty
+    
+print(map_func(square, l))
+print(map_func(lambda a: a**2, l))
+
+# map() and map_func both do the same work
