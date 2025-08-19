3d616151b85f667aa9e27b57806f51f0e272258c
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-19 11:15:53 +0530

    function returning function

--- Python/chapter14/T167_FunctionReturningFunction.py
+++ Python/chapter14/T167_FunctionReturningFunction.py
@@ -2,18 +2,14 @@
 
 def outer_func():
     def inner_func():
-        print('Inside inner Func')
+        print('inside inner func')
     return inner_func()
 
 outer_func()
 
-
 def outer_func2(msg):
     def inner_func2():
-        print(f'message is: {msg}')
-    return inner_func2
+        print(f"message is : ", msg)
+    return inner_func2()
 
-
-var2 = outer_func2('Hello')
-
-var2()
+outer_func2("Hello")
