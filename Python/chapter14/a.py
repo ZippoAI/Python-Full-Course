edee435c2b6fc545d9c5ea1e0fd389d500226ae2
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-20 12:30:20 +0530

    decorator practice

---
+++ Python/chapter14/a.py
@@ -0,0 +1,22 @@
+
+def decorator_func(any_func):
+    def wrapper_func():
+        print('This is wrapper Function')
+        any_func()
+    return wrapper_func()
+
+
+@decorator_func
+def func1():
+    print('This is function 1')
+
+func1()
+
+
+
+
+def func2():
+    print('This is function 2')
+
+var = decorator_func(func2)
+var()
