cd8fcf26f8924cae9f94a70d5c99e9a8d422c344
ZiPPO7777 <iamrealzippo@gmail.com>
2025-08-27 16:45:33 +0530

    a

--- Python/chapter14/a.py
+++ Python/chapter14/a.py
@@ -1,19 +1,25 @@
 
+from functools import wraps
 
 def decorator_func(any_func):
-    def wrapper_func(*args, **kwargs):
+    @wraps(any_func)
+    def wrapper_function(*args, **kwargs):
+        '''
+        This is wrapper function
+        '''
         print('This is awesome function')
         return any_func(*args, **kwargs)
-    return wrapper_func
-
-@decorator_func
-def func(a):
-    print('This is function with argument: ', a)
+    return wrapper_function
 
 
-func(7)
+
+
 @decorator_func
 def add(a, b):
+    '''
+    This is add function
+    '''
     return a+b
 
-print(add(5,5))
+print(add.__doc__)
+print(add.__name__)
