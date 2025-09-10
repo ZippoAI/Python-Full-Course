34a968b713b5cee1349b790bbf2b36e2d9c19f44
ZiPPO7777 <iamrealzippo@gmail.com>
2025-09-10 13:45:41 +0530

    intro to OOP

---
+++ Python/chapter16-OOP/OOP_Intro.py
@@ -0,0 +1,21 @@
+# Object oriented Programming
+
+# Most common word in OOP
+# class , object(instance), method 
+
+# list class
+# list object
+# method
+
+
+
+l = [1,2,3,4,5]
+l2 = [2,34,5,6]
+
+l.append(8)
+
+print(l)
+
+
+
+
