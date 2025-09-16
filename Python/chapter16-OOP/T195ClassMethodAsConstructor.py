e13799f73500b297a6d12fc88830327d805e7c2e
ZiPPO7777 <iamrealzippo@gmail.com>
2025-09-16 14:13:32 +0530

    class method as a constructor

---
+++ Python/chapter16-OOP/T195ClassMethodAsConstructor.py
@@ -0,0 +1,45 @@
+#Class Method as Constructor
+
+
+class Person:
+    count_instance = 0
+    def __init__(self, first_name, last_name, age):
+        Person.count_instance+=1
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    @classmethod
+    def from_string(cls, string):
+        first,last,age = string.split(',')
+        return cls(first,last,int(age))
+
+
+    @classmethod
+    def total_instance(cls):
+        return f"You have created {cls.count_instance}"
+
+
+    def __str__(self):
+        if self.age>=18:
+            message = "You are above 18"
+        else:
+            message = 'You are below 18'
+        return f"{self.first_name} {self.last_name}\n{message}"
+    
+    
+        
+# Usually, we create an object like this:
+person1 = Person('Bulbul', 'Hassan', 18)
+
+# But if we want to pass all the data in a single string,
+# we can do something like this:
+p3 = Person.from_string('Bulbul, HassaM, 24')
+
+
+# print(person1)
+print(p3)
+
+
+print(Person.total_instance())
