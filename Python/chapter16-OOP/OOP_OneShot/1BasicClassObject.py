fb894d7fecb9827bc8cd66e84676cf85e9a8cb47
ZiPPO7777 <iamrealzippo@gmail.com>
2025-10-07 00:26:02 +0530

    revision2

--- Python/chapter16-OOP/OOP_OneShot/1BasicClassObject.py
+++ Python/chapter16-OOP/OOP_OneShot/1BasicClassObject.py
@@ -2,14 +2,20 @@ class Car:
     def __init__(self, user_brand, user_model):
         self.brand = user_brand
         self.model = user_model
+
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+
+
         
         
 
 my_car = Car("BMW", "M8")
 
-print(my_car.brand)
-print(my_car.model)
+print(my_car.full_name())
+
 
 my_new_car = Car("Tata", "Safari")
 
-print(my_new_car.model)
+# print(my_new_car.model)
