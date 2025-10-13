c8dfb15f89914b6239859654ece617401ed60496
ZiPPO7777 <iamrealzippo@gmail.com>
2025-10-13 16:16:00 +0530

    A

--- revise/1.py
+++
@@ -1,8 +0,0 @@
-def missing_char(str, n):
-    return str.replace(str[n], '', 1)
-
-
-print(missing_char("jitendrad", 5))
-missing_char('kitten', 1)
-missing_char('kitten', 0)
-missing_char('kitten', 4)
