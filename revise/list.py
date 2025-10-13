301eb2121da226b267c2d18bf3a6ef0aa33f2380
ZiPPO7777 <iamrealzippo@gmail.com>
2025-10-13 16:15:46 +0530

    a

--- revise/list.py
+++ revise/list.py
@@ -1,49 +1,21 @@
-number1 = [[41,51,61],[71,81,91]]
+fruits = ['mango', 'apple']
 
-empty_list = []
+fruits.insert(0,'Orange')
 
-for sublist in number1:
-    temp =[]
-    for i in sublist:
-        rev1 = str(i)[::-1]
-        temp.append(int(rev1))
-    empty_list.append(temp)
-print(empty_list)
+print(fruits)
 
 
-print("-----------------------")
+fruits1 = ['Apple', 'kiwi']
+
+fruits2 = ['Orange', 'Kola']
+
+fruits1.extend(fruits2)
+print(fruits1)
 
 
+# Delete Data from list
 
-number2 = [41,51,61]
-temp2 = []
-for i in number2:
-    rev2 = str(i)[::-1]
-    temp2.append(int(rev2))
-print(temp2)
-
-
-
-print("-----------------------")
-
-
-number3 = [12345]
-
-for i in number3:
-    rev3 = int(str(i)[::-1])
-    print(rev3)
-
-
-print("-----------------------")
-
-
-number5 = [[41,51,61],[71,81,91]]
-empty_l = []
-for sublist in number5:
-    temp= []
-    for i in sublist:
-        rev4 = int(str(i)[::-1])
-        temp.append(rev4)
-    empty_l.append(temp)
-print(empty_l)
-    
+fruits = ['mango', 'apple', 'Apple', 'kiwi']
+print()
+fruits.pop()
+print(fruits)
