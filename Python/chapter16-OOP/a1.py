d7ff456dee335d9b9300c35fedc5c88802b3ccd2
ZiPPO7777 <iamrealzippo@gmail.com>
2025-10-13 22:47:24 +0530

    OOP Revision

---
+++ Python/chapter16-OOP/a1.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+
+
+p1 = Person('Bulbul', 'Hassan', 21)
+p2 = Person('Zippo', 'Gaming', 20)
+
+print(p1.first_name)
+print(p2.first_name)
