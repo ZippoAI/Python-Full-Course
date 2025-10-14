6c083d768bb69ba887665195efff85e5d6c187b2
ZiPPO7777 <iamrealzippo@gmail.com>
2025-10-15 01:50:29 +0530

    class variable part 2

--- Python/chapter16-OOP/a.py
+++ Python/chapter16-OOP/a.py
@@ -5,18 +5,21 @@ class Laptop:
         self.model_name = model_name
         self.price = price
 
-    def DiscountPrice(self):
-        discount_amount = (Laptop.sale_discount/100)*self.price
-        return f"Total - {self.price} Discount - {Laptop.sale_discount}%({discount_amount} Rs)\nFinal Price - {self.price - discount_amount}\n"
+    def ApplyDiscount(self):
+        discount_amount = (self.sale_discount/100)*self.price
+        return f"Total - {self.price} Discount - {self.sale_discount}%({discount_amount} Rs)\nFinal Price - {self.price - discount_amount}\n"
 
 
         
 
 l1 = Laptop('Asus', 'Tuf', 200)
 l2 = Laptop('Acer', 'Nitro', 670)
+l2.sale_discount = 50
+print(l2.__dict__)
+print(l2.ApplyDiscount())
 
-print(l1.brand_name)
 
-print(l2.DiscountPrice())
+
+# print(l2.ApplyDiscount())
 
 print()
--- Python/chapter16-OOP/a1.py
+++ Python/chapter16-OOP/a1.py
@@ -1,16 +1,22 @@
-# Class Variable
+class Laptop:
+    sale_discount = 20
+    def __init__(self, brand_name, model_name, price):
+        self.brand_name = brand_name
+        self.model_name = model_name
+        self.price = price
 
-class Circle:
-    pi = 3.14
-    def __init__(self, radius):
-        self.radius = radius
+    def DiscountPrice(self):
+        discount_amount = (Laptop.sale_discount/100)*self.price
+        return f"Total - {self.price} Discount - {Laptop.sale_discount}%({discount_amount} Rs)\nFinal Price - {self.price - discount_amount}\n"
+
+
         
 
-    def calc_circumference(self):
-        return 2*Circle.pi*self.radius
-    
+l1 = Laptop('Asus', 'Tuf', 200)
+l2 = Laptop('Acer', 'Nitro', 670)
 
-c = Circle(4)
-c2 = Circle(5)
+print(l1.brand_name)
 
-print(c.calc_circumference())
+print(l2.DiscountPrice())
+
+print()
